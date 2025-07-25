d228ebcff6f6f40b63df79d3ff496d229023e24d
Burhanuddin Vora <burhanuddin.vora@gmail.com>
2025-07-25 14:09:07 +0530

    Boiler plate for react UI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+export default function App() {
+  return (
+    <div>
+      <Button className='flex flex-col items-centre justify-center border border-red-500'>
+        Click me
+      </Button>
+    </div>
+  )
+}
